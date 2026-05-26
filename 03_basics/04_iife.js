05b04b3620cd201d1748debac0139075cf566683
Komal Rathore <24.a09.027@gmail.com>
2026-05-26 19:36:46 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+//Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})(); // remeber semicolon
+
+((name)=>{
+    console.log(`DB connected Two ${name} `)
+})('komal')
