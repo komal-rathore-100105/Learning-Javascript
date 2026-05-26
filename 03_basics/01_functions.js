ce649bf20d1b0fdff7a6caaae082f280c17d9267
Komal Rathore <24.a09.027@gmail.com>
2026-05-26 15:20:18 +0000

    objects again

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -16,3 +16,30 @@ function loginUserMessage(username="sam"){
 //console.log(loginUserMessage("komal"))
 
 console.log(loginUserMessage()) // undefined
+
+function calcCartPrice(val1,val2, ...num1){
+    return num1;
+}
+// console.log(calcCartPrice(200,400,500,2000))
+
+console.log(loginUserMessage("komal"))
+
+const user ={
+    username: "komal",
+    price : 188  
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+// prices - undefined
+handleObject(user)
+
+const MyNewArray = [200, 400, 100, 600]
+
+function return2ndValue(myArray){
+    return myArray[1]
+}
+
+console.log(return2ndValue(MyNewArray))
+console.log(return2ndValue([200, 400, 100, 600]))
